0b6e50bcf02657cead6afe7de7c1dd2f8028bddb
yakudzam <yakudzam@gmail.com>
2012-10-28 18:09:46 +0200

    added student and group forms

--- group/views.py
+++ group/views.py
@@ -1,12 +1,40 @@
 # Create your views here.
 from django.template import RequestContext
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from models import Group
+from django.contrib.auth.decorators import login_required
+from forms import GroupForm
 
-def group(request, group_id):
-    group = get_object_or_404(Group, pk=group_id)
-    students = group.students.all()
-    data = dict(group=group, students=students)
+def grouplist(request):
+    groups = Group.objects.all()
+    data = dict(groups=groups)
     return render_to_response('group/group.html',
+                              data,
+                              context_instance=RequestContext(request))
+    
+@login_required(login_url='/login/')
+def manage_group(request, group_id=None):
+
+    if group_id:
+        group = get_object_or_404(Group, pk=group_id)
+    else:
+        group = None
+
+    if request.method == 'POST':
+        if group:
+            form = GroupForm(request.POST, instance=group)
+        else:
+            form = GroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        if group:
+            form = GroupForm(instance=group)
+        else:
+            form = GroupForm()
+
+    data = dict(form=form, group_id=group_id)
+    return render_to_response('group/add_group.html',
                               data,
                               context_instance=RequestContext(request))
--- student/views.py
+++ student/views.py
@@ -1,10 +1,46 @@
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from group.models import Group
+from models import Student
+from django.contrib.auth.decorators import login_required
+from forms import StudForm
 
-def studentlist(request):
-    groups = Group.objects.all()
-    data = dict(groups=groups)
+def studentlist(request, group_id):
+    group = get_object_or_404(Group, pk=group_id)
+    students = group.students.all()
+    data = dict(group=group, students=students)
     return render_to_response('student/student.html',
+                              data,
+                              context_instance=RequestContext(request))
+    
+@login_required(login_url='/login/')
+def manage_student(request, student_id=None, group_id=None):
+    if student_id:
+        student = get_object_or_404(Student, pk=student_id)
+    else:
+        student = None
+
+    if group_id:
+        group = get_object_or_404(Group, pk=group_id)
+    else:
+        group = None
+        
+    if request.method == 'POST':
+        if student:
+            form = StudForm(request.POST, instance=student)
+        else:
+            form = StudForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        if student:
+            form = StudForm(instance=student)
+        else:
+            form = StudForm(initial={'student_group': group})
+            
+    data = dict(form=form, student_id=student_id)
+    return render_to_response('student/add_student.html',
                               data,
                               context_instance=RequestContext(request))
